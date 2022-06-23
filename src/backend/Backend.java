b44aa298b6b08b896ab8a2a038bf94d7649660d2
Ulrich Meisl <meisl.ulrich@gmail.com>
2022-06-23 14:39:07 +0200

    implement crude Filter
    there is now a crude filter

--- src/backend/Backend.java
+++ src/backend/Backend.java
@@ -1,6 +1,9 @@
 package backend;
 
 import java.io.IOException;
+import java.util.ArrayList;
+//import java.util.Scanner;
+
 import org.json.*;
 
 
@@ -8,8 +11,80 @@ public class Backend {
 
 	
 	public static void main(String[] args) {
+		ArrayList<JSONObject> jsonObj = new ArrayList<JSONObject>();
+		ArrayList<JSONObject> outputObj = new ArrayList<JSONObject>();
 		
-		JSONObject jsonObj = new JSONObject();;
+		/*
+		Scanner filterScan = new Scanner(System.in);
+		String filterKey = "";
+		String filterValue = "";
+		
+		if(filterScan.hasNextLine())
+		{
+			filterKey = filterScan.nextLine();
+			if(filterScan.hasNextLine())
+			{
+				filterValue = filterScan.nextLine();
+			}
+		}
+		filterScan.close();
+		
+		
+		System.out.println(filterKey);
+		
+		System.out.println(filterValue);
+		*/
+		
+		//crude Filter
+		
+		String crudeFilterKey = "";
+		crudeFilterKey = "d"; // department
+		//crudeFilterKey = "s"; //source
+	
+		
+		String filterValue = "";
+		
+		filterValue = "Municipal Transportation Agency";
+		
+		/*
+		 * departments
+		Municipal Transportation Agency
+		Asian Art Museum
+		Animal Care and Control
+		Adult Probation Department
+		Arts Commission
+		Assessor-Recorder
+		Board of Supervisors
+		City Attorney
+		Community College District
+		Convention Facilities Department
+		Controller’s Office
+		Planning
+		County of SF Superior Court (Trial Courts)
+		Central Shops
+		Civil Service Commission
+		Child Support Services
+		District Attorney
+		Building Inspection
+		Emergency Management
+		Human Resources
+		Public Health
+		
+		 * source_type
+		B100
+		Diesel
+		CNG
+		Propane
+		B20
+		B5
+		Gasoline
+		Natural Gas
+		Electric
+		Steam
+		
+		*/
+		
+		
 		try {
 			jsonObj = JSONReader.readJsonFromUrl("https://data.sfgov.org/resource/pxac-sadh.json");
 		} catch (JSONException | IOException e) {
@@ -18,14 +93,36 @@ public class Backend {
 		}
 
 		
-		System.out.println(jsonObj.toString());
+		if(crudeFilterKey == "d")
+		{
+			for(JSONObject j : jsonObj)
+			{
+				if(j.getString("department").compareTo(filterValue) == 0)
+				{
+					outputObj.add(j);
+				}
+			}
+		}
+		else if(crudeFilterKey == "s")
+		{
+			for(JSONObject j : jsonObj)
+			{
+				if(j.getString("source_type").compareTo(filterValue) == 0)
+				{
+					outputObj.add(j);
+				}
+			}
+		}
+		else
+		{
+			outputObj = jsonObj;
+		}
 		
+		System.out.println(outputObj.size());
 		
-		/*
-		JSONObject json = new JSONObject();
-		json.append("key", "test");
-		System.out.println(json.toString());
-		*/
+		//System.out.println(jsonObj.toString());
+		System.out.println(outputObj.toString());
+	
 	}
 
 }
--- src/backend/JSONReader.java
+++ src/backend/JSONReader.java
@@ -5,30 +5,49 @@ import java.io.*;
 import org.json.*;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JSONReader {
 	
-	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	public static ArrayList<JSONObject> readJsonFromUrl(String url) throws IOException, JSONException {
+		
+		ArrayList<JSONObject> jsonObjectList = new ArrayList<JSONObject>();
+		String[] keys = new String[] {"department", "source_type", "emissions_mtco2e"};
+		
 	    InputStream in = new URL(url).openStream();
 	   
 	    Scanner scan = new Scanner(in);
 	    String jsonText = scan.useDelimiter("\\Z").next();
+	    
 	    jsonText = jsonText.replace('[', ' ');
 	    jsonText = jsonText.replace(']', ' ');
-	    //jsonText = jsonText.replace(' ', '_');
+	    
 	    scan.close();
 	    
 	    scan = new Scanner(jsonText);
 	    
-	    String jj = scan.nextLine().substring(1);
 	    
-	    System.out.println(jj);
+	    String line;
+	    JSONObject jsonTest;
+	    Double co2e;
+	    
+	    while (scan.hasNextLine())
+	    {
+	    	line = scan.nextLine().substring(1);
+	    	jsonTest = new JSONObject(line);
+	    	co2e = Double.parseDouble(jsonTest.getString("emissions_mtco2e"));
+	    	
+	    	
+	    	if(co2e > 0.0)
+		    {
+		    	jsonObjectList.add(new JSONObject(jsonTest, keys));
+		    }
+	    }
 	    
 	    scan.close();
 	    
-	    //System.out.println(jsonText);
 	    
-	    return new JSONObject(jsonText);
+	    return jsonObjectList;
 	}
 }
